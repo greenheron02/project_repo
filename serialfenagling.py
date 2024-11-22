221c217807e30f4ddb1195b395ec840ce169ec6b
greenheron02 <122500718+greenheron02@users.noreply.github.com>
2024-11-22 16:23:03 -0600

    Update serialfenagling.py
    Changed from pynput to xdotool and subprocess
    
    Xdotool uses the X11 XTERM extension, along with others, to interact natively with the Linux environment.

--- serialfenagling.py
+++ serialfenagling.py
@@ -1,20 +1,59 @@
-import serial 
-from pynput import keyboard
-from time import sleep
-ser = serial.Serial('/dev/ttyACM0')
-print(ser.name)
-print("hamburgers")
-counter = 1
-keycontrol = keyboard.Controller()
-while True: 
-    x = ser.readline()
-    x = x.decode("utf-8")
-    # print(x[:-2] == 'a')
-    x = x[:-2]
-    if (x == 'a'): 
-        keycontrol.press('a')
-    elif (x == 'b'):
-        keycontrol.press('b')
-    print(f"{counter}: {x[:-1]}")
-    counter = counter + 1
+import subprocess
+import serial
+import serial.serialutil 
 
+try: 
+    ser = serial.Serial('/dev/ttyACM0')
+except serial.serialutil.SerialException:
+    print('No USB present idjit') 
+
+
+def press_key(key): 
+    subprocess.run(["xdotool","key",key])
+
+def touch_key(key): 
+    subprocess.run(["xdotool","keydown",key])
+
+def release_key(key): 
+    subprocess.run(["xdotool","keyup",key])
+
+    
+
+
+ke_ys = {
+        'up':'0xff52', 
+        'down':'0xff54', 
+        'left':'0xff51',
+        'right':'0xff53',
+        'a':'0x0061', 
+        'b':'0x0062', 
+        'w':'0x0077',
+        's':'0x0073',
+        'd':'0x0064'
+    }
+
+done = False 
+
+while not done: 
+    x = ser.readline()
+    x = x.decode()
+    x = x[:-2] 
+    
+    keys_input = {"U":'up', "D":'down', 
+             "L":'left', "R":'right',
+             "a":'a', "b":'w',
+             "c":'d', "d":'s'}
+    cmd = x[:-2] 
+    x = x[4:]
+    print(f"type: {cmd}, x: {x}")
+    # print(len(cmd))
+
+
+    # if x in keys_input.keys(): 
+    #     x = keys_input[x]
+    #     k = ke_ys[x]
+    #     print(f"pressed key: {k}")
+    #     if (cmd == "hld"): 
+    #         touch_key(k)
+    #     if (cmd == "rel"): 
+    #         release_key(k)
